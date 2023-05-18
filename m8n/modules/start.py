9e2a6bc4ee95f26cfffb5b62fc364002a562ea10
gshshshvq <131294642+gshshshvq@users.noreply.github.com>
2023-05-18 06:19:30 +0300

    Update start.py

--- m8n/modules/start.py
+++ m8n/modules/start.py
@@ -16,7 +16,7 @@ from m8n.config import OWNER_USERNAME
 
 
 
-@Client.on_message(command("/start") & filters.private & ~filters.edited)
+@Client.on_message(command("") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"{START_PIC}",
